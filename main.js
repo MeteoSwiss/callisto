a0addf2193de1d399d268ae408ac2204fe7e2e00
wolfidan <wolfidan@users.noreply.github.com>
2025-05-05 18:51:57 +0000

    deploy: becdaf4bd17237669670b9b617a5ab224927e745

---
+++ main.js
@@ -0,0 +1,26 @@
+let imageDates = [];
+let index = 0;
+
+function updateImage() {
+  document.getElementById("plot").src = imageNames[index];
+}
+
+function prevImage() {
+  if (index > 0) { index--; updateImage(); }
+}
+
+function nextImage() {
+  if (index < imageDates.length - 1) { index++; updateImage(); }
+}
+
+function loadPage(name) {
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("images/"+name+".json")
+    .then(response => response.json())
+    .then(data => {
+      imageNames = data;
+      index = imageNames.length - 1; // most recent
+      updateImage();
+    });
+});
+}
